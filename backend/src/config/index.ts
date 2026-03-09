117c58807ba6b8798d4d90f367cdf94b98c2eb78
NIKITA2000 <someshkin2103@gmail.com>
2026-03-09 12:32:54 +0000

    изменено время действия JWT токена на 100 лет

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -7,7 +7,7 @@ export const config = {
 
   jwt: {
     secret: process.env.JWT_SECRET || "fallback_secret_change_me",
-    expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+    expiresIn: process.env.JWT_EXPIRES_IN || "100y",
   },
 
   livekit: {
